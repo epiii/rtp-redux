b78c2a100e6a11e381c83d3bb6cd9564f23023c4
epiii <elfri.app@gmail.com>
2020-01-09 21:11:49 -0800

    Updates

---
+++ precache-manifest.b8259fdc4394b0ea86a52c82bfdaf673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f3b56354b59ed84f0b593b0e335ce6",
+    "url": "/rtp-redux/index.html"
+  },
+  {
+    "revision": "208aea18084f871b292e",
+    "url": "/rtp-redux/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "84f6d5da01da07aa57ef",
+    "url": "/rtp-redux/static/css/main.1cb039ba.chunk.css"
+  },
+  {
+    "revision": "208aea18084f871b292e",
+    "url": "/rtp-redux/static/js/2.ac716f75.chunk.js"
+  },
+  {
+    "revision": "0301e2a03621301b384cbb24cb24977c",
+    "url": "/rtp-redux/static/js/2.ac716f75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84f6d5da01da07aa57ef",
+    "url": "/rtp-redux/static/js/main.c1f4918a.chunk.js"
+  },
+  {
+    "revision": "fad62270ecc955ad0517",
+    "url": "/rtp-redux/static/js/runtime-main.a56b61f6.js"
+  }
+]);
